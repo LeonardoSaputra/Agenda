38b0c3c10bdb28daebd6367de4cf6e7cbae881ce
LeonardoSaputra <100183529+LeonardoSaputra@users.noreply.github.com>
2024-02-27 18:21:56 +0700

    First Commit
    First Commit

---
+++ LoginPage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+    <body>
+        <form method = "POST" enctype = "multipart/form-data" ><br>
+        <!-- input Username -->
+        <div>
+            <label for="Username">Username</label><br>
+            <input type = "text" name = "username"/>
+        </div>
+            
+        <br>
+        <div>
+            <label for="Password">Password</label><br>
+            <input type = "password" name ="password" />
+
+        </div>
+        <!-- Input Password -->
+        <br>
+        <!-- Button -->
+        <button type = "Submit" value = "Submit">Login </submit>
+
+        </form>
+    </body>
+
+</html>
+
