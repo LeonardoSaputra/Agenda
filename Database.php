0ffa3098cdb001da99104759cf68fa7305872c47
LeonardoSaputra <leonardo.saputra94@gmail.com>
2024-02-28 12:33:42 +0700

    mengganti username dan password menjadi "root" dan ""

--- Database.php
+++ Database.php
@@ -1,24 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "admin" or "user";
-$password = "admin"  or "user";
+$username = "root";
+$password = "";
 $dbname = "secprogproject";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Fungsi registrasi pengguna
-function loginUser($username, $password) {
-    global $conn;
-    $sql = "SELECT * FROM users WHERE username='$username'";
-    $result = $conn->query($sql);
-    if ($result->num_rows == 1) {
-        $user = $result->fetch_assoc();
-        if (password_verify($password, $user['password'])) {
-            return $user;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
-}
 ?>
