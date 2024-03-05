17682d7e8840b09741edc16ea13582368c157fc4
LeonardoSaputra <leonardo.saputra94@gmail.com>
2024-03-05 23:12:02 +0700

    memindahkan codingan html ke file tersendiri dan  menambahkan session pada username

--- LoginPage.php
+++ LoginPage.php
@@ -5,20 +5,22 @@ include "Database.php";
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
+    
     // Query untuk mencari pengguna dengan username yang sesuai
-    $sql = "SELECT * FROM users WHERE username = '$username'and password = '$password'";
+    $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        // $username = $result->fetch_assoc();
+        
         // Memeriksa apakah password cocok
         // if (password_verify($password, $username['password'])) {
         //     echo "Login berhasil!";
-        //     $_SESSION['isLoggedIn'] = true;
-        //     $_SESSION['username'] = $username;
-        //     // $_SESSION['role'] = $username['role']; 
-        //     header("Location: welcome.php");
+            $_SESSION['isLoggedIn'] = true;
+            $_SESSION['username'] = $username;
+            $role = $_POST['role'];
+            $_SESSION['role'] = $role; 
+            $username = $result->fetch_assoc();
+            header("Location: welcome.php");
         //     exit;
         // } else {
         //     echo "Login gagal! Password salah.";
@@ -27,29 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "Login gagal! Pengguna tidak ditemukan.";
     }
 }
-session_destroy();
+// session_destroy();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <form method="POST">
-        <div>
-            <label for="Username">Username</label><br>
-            <input type="text" name="username"/>
-        </div>
-            
-        <div>
-            <label for="Password">Password</label><br>
-            <input type="password" name="password" />
-        </div>
-        
-        <button type="submit" value="Submit">Login</button>
-    </form>
-</body>
-</html>
+
