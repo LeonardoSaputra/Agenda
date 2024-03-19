530ef193c082b8b267e06c5f68f4dc5fb0d9ae6c
LeonardoSaputra <leonardo.saputra94@gmail.com>
2024-03-20 02:15:13 +0700

    mengganti link dari loginpagephp ke loginpagehtml

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forum Thread</title>
+    <title>Agenda Perusahaan</title>
 </head>
 <body>
-    <a href="LoginPage.php">Login</a>
+    <a href="login_page.html">Login</a>
    
 </body>
 
