fbbc78fa38ed4070ef41c307b967e6d7be7073ca
LeonardoSaputra <leonardo.saputra94@gmail.com>
2024-05-21 22:58:00 +0700

    perbaikan coding karena variabel content saat diinput tidak terdetect pada database

--- add_agenda.php
+++ add_agenda.php
@@ -3,10 +3,11 @@ include "Database.php";
 
 // Ambil data dari formulir
 $agenda = $_POST['title'];
+$content = $_POST['content'];
 $tanggal = $_POST['tanggal'];
 
 // Query untuk menambahkan agenda baru
-$sql = "INSERT INTO agenda (title, date) VALUES ('$agenda', '$tanggal')";
+$sql = "INSERT INTO agenda (title, content, date) VALUES ('$agenda', '$content', '$tanggal')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Agenda berhasil ditambahkan <br> <br>";
