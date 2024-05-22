2268d0348122909c2ed872c3578c0214f455830c
LeonardoSaputra <leonardo.saputra94@gmail.com>
2024-05-22 20:26:47 +0700

    penambahan verifikasi password dengan password hash

--- LoginController.php
+++ LoginController.php
@@ -5,33 +5,33 @@ include "Database.php";
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $role = $_POST['role'];
+    // $role = $_POST['role'];
     // Query untuk mencari pengguna dengan username yang sesuai
-    $stmt = $conn->prepare("SELECT username, password, role FROM users WHERE username = ?");
+    $stmt = $conn->prepare("SELECT username, hashed_password, role FROM users WHERE username = ?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
-        $username = $result -> fetch_assoc();
         // Memeriksa apakah password cocok
-        // if (password_verify($password, $username['password'])) {
-        //     echo "Login berhasil!";
-            // $_SESSION['isLoggedIn'] = true;
-            $_SESSION['username'] = $username['username'];
+        $users = $result -> fetch_assoc();
+        $stored_password_hashed = $users['hashed_password'];
+        if (password_verify($password, $stored_password_hashed)) {
+                echo "Login berhasil!";
+            $_SESSION['isLoggedIn'] = true;
+            $_SESSION['username'] = $users['username'];
             
-            $_SESSION['role'] = $username['role']; 
+            $_SESSION['role'] = $users['role']; 
             header("Location: welcome.php");
-        //     exit;
-        // } else {
-        //     echo "Login gagal! Password salah.";
-        // }
+            exit;
+        } else {
+            echo "Login gagal! Password salah.";
+            echo '<br> <a href="login_page.html">Back</a>';
+        }
     } else {
         echo "Login gagal! Pengguna tidak ditemukan.";
         echo '<br> <a href="login_page.html">Back</a>';
     }
 }
-
 ?>
-
 
