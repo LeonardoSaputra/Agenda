f363e29279b05baea5428fec10483c754c750372
LeonardoSaputra <leonardo.saputra94@gmail.com>
2024-05-22 20:26:09 +0700

    memodifikasi code untuk menghindari SQL Injection

--- filter_dataBulan.php
+++ filter_dataBulan.php
@@ -5,18 +5,24 @@ include "Database.php";
 
 if (isset($_GET['bulan'])) {
     $bulan = $_GET['bulan'];
-    $query = "SELECT * FROM agenda WHERE MONTH(date) = $bulan";
+    $query = "SELECT * FROM agenda WHERE MONTH(date) = ?";
+    
+    // Siapkan statement untuk menghindari SQL Injection
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $bulan);
+    $stmt->execute();
+    $result = $stmt->get_result();
     // Lakukan query ke database untuk mendapatkan data yang sesuai dengan bulan yang dipilih
 
-        $result = mysqli_query($conn, $query);
+        // $result = mysqli_query($conn, $query);
         //Mengolah dan menampilkan data
         if (mysqli_num_rows($result) > 0) {
             // Output data dari setiap baris
             while ($row = mysqli_fetch_assoc($result)) {
-                echo "ID: " . $row["id"] . "<br>";
-                echo "Title: " . $row["title"] . "<br>";
-                echo "Content: " . $row["content"] . "<br>";
-                echo "Date: " . $row["date"] . "<br>" . "<br>";
+                echo "ID: " . htmlspecialchars($row["id"]) . "<br>";
+                echo "Title: " . htmlspecialchars($row["title"]) . "<br>";
+                echo "Content: " . htmlspecialchars($row["content"]) . "<br>";
+                echo "Date: " . htmlspecialchars($row["date"]) . "<br>" . "<br>";
             }
             echo '<br> <a href="welcome.php">Back</a>' ;
         }
